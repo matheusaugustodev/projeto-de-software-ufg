7b9b05686c21cd17acf54a59572cbca33fda3622
mronald-js <mronaldjs@gmail.com>
2024-11-08 22:11:33 -0300

    mudanças no comportamento da navBar e mudança de imagem no perfil

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,40 +1,39 @@
-"use client"
+"use client";
+import { useState, useEffect } from "react";
 import { IoMdSearch } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
-//get the actual route
-import { useRouter } from 'next/navigation';
-
-//Array of pathnames
-const pathnames = ['/servicos', '/votacao', '/parceiro', '/sobre'];
-
-function currentLink(pathname) {
-    if (pathnames.includes(pathname)) {
-        return pathnames.indexOf(pathname);
-    }
-    return -1;
-}
-function styleLink(index, pathname) {
-    if (currentLink(pathname) === index) {
-        return "border-b-2 border-blue-500 text-gray-400";
-    }
-    return "";
-}
-
+import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
+    // Array de pathnames
+    const pathnames = ['/servicos', '/votacao', '/parceiro', '/sobre'];
+    const pathname = usePathname(); // Obtém o pathname atual
+    const [activeIndex, setActiveIndex] = useState(-1);
 
-    const { pathname } = useRouter();
-    const current = styleLink(currentLink(pathname), pathname);
+    useEffect(() => {
+        // Atualiza o índice ativo com base no pathname atual
+        const currentIndex = pathnames.indexOf(pathname);
+        setActiveIndex(currentIndex);
+    }, [pathname]);
+
+    function styleLink(index) {
+        // Adiciona a classe ativa com base no índice
+        return index === activeIndex ? "border-b-2 border-blue-500 text-gray-400" : "";
+    }
 
     return (
         <nav className="bg-white shadow w-full flex items-center justify-evenly">
             <span className="">LOGO</span>
             <nav className="h-16 w-11/12 flex items-center justify-evenly font-bold">
                 <ul className="flex gap-28 items-center">
-                    <li className={current}><a href="/servicos">Servicos</a></li>
-                    <li><a href='/'>Votação de propostas</a></li>
-                    <li><a href="/">Seja Parceiro</a></li>
-                    <li><a href="/">Sobre</a></li>
+                    {pathnames.map((path, index) => (
+                        <li
+                            key={index}
+                            className={styleLink(index)}
+                        >
+                            <a href={path}>{path.replace('/', '')}</a>
+                        </li>
+                    ))}
                     <li><IoMdSearch className="h-8 w-8"/></li>
                     <li>
                         <a href="/perfil">
@@ -44,5 +43,5 @@ export default function NavBar() {
                 </ul>
             </nav>
         </nav>
-    )
+    );
 }
