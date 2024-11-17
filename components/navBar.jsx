c1bd7788a5a0416862d0128efb388f687360c16a
mronald-js <mronaldjs@gmail.com>
2024-11-17 16:55:28 -0300

    ...

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { IoMdSearch } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import { usePathname } from 'next/navigation';
 
