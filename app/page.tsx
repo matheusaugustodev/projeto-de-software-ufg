c208aa6d080b5fcae68f6ea92275303fac454ee5
mronald-js <mronaldjs@gmail.com>
2024-11-18 19:32:54 -0300

    fix fix fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import NavBar from "@/components/navBar";
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Home
-      </h1>
+    <div className="bg-customBlue h-screen">
+
+      <NavBar />
+      <main className="">
+        <h1 className="p-12 text-xl text-white">Home</h1>
+      </main>
     </div>
   );
 }
