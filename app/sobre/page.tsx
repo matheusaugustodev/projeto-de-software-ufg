812a3009d9e7d40391cdc8ee2ae6c8f2cbc9b795
mronald-js <mronaldjs@gmail.com>
2024-12-10 18:17:42 -0300

    correção nas pages de login e acréscimo de página de regras

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -5,7 +5,6 @@ export default function Sobre() {
               
             <section className="bg-customBlue h-screen p-5 flex flex-col items-center">
                 <h1 className="text-white text-4xl font-bold">Sobre nós</h1>
-                <p className="text-white text-lg mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, asperiores aliquam. Labore vitae nulla optio minus! Quia dolorum ipsa exercitationem fugiat consectetur quasi corrupti, nesciunt vel iste veritatis. Tempora, dignissimos!</p>
             </section>
         </>
     )
