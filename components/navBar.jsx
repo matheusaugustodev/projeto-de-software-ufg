cec1b50a609add0ba3afa12afd33da7d13f9bb16
Matheus Augusto <125043947+matheusaugustodev@users.noreply.github.com>
2024-12-11 15:41:28 -0300

    Revert "correção nas pages de login e acréscimo de página de regras"

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -29,7 +29,7 @@ export default function NavBar() {
     }
 
     return (
-        <nav className="bg-white text-black shadow w-full flex justify-center items-center">
+        <nav className="bg-white shadow w-full flex justify-center items-center">
             <nav className="h-16 w-11/12 flex items-center justify-between font-bold">
                 <Link href="/" title="Home">
                     <Image
